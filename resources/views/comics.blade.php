5c259f12ebe24534287fbab95f261a1bdbdc0c92
Alfredodaniello <alfredo_daniello@libero.it>
2024-05-20 19:36:55 +0200

    feat: add data on web.php and use foreach for cards

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    ciao sono la pagina dei fumetti
+    <section id="comics">
+        <div class="container">
+            <div class="row">
+                
+                    @foreach ($comics as $comic)
+                    <div class="col-2 d-flex">
+                        <div class="card my-2">
+                            <div class="card-img">
+                                <img src="{{ $comic['thumb']}}" alt="">
+                            </div>
+                            <div class="text-center my-1">
+                                {{$comic['series']}}
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+                
+            </div>
+        </div>
+    </section>
 @endsection
