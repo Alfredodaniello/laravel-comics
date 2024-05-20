43f4502e27399e6349134d36c489f35ba7a9e641
Alfredodaniello <alfredo_daniello@libero.it>
2024-05-20 18:58:03 +0200

    feat: add header.blade

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,40 @@
+<header class="d-flex align-items-center justify-content-between">
+    <div class="logo">
+        <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="">
+    </div>
+    <nav>
+        <ul class="list-unstyled d-flex gap-3 text-uppercase">
+            <li>
+                <a href="#">characters</a>
+            </li>
+            <li>
+                <a href="/comics">comics</a>
+            </li>
+            <li>
+                <a href="#">movie</a>
+            </li>
+            <li>
+                <a href="/tv">tv</a>
+            </li>
+            <li>
+                <a href="#">collectibles</a>
+            </li>
+            <li>
+                <a href="#">games</a>
+            </li>
+            <li>
+                <a href="#">video</a>
+            </li>
+            <li>
+                <a href="#">fans</a>
+            </li>
+            <li>
+                <a href="#">new</a>
+            </li>
+            <li>
+                <a href="#">shop</a>
+            </li>
+
+        </ul>
+    </nav>
+</header>
